17d5fa92a29c46a1317b9b938f9579bb59c388ac
Gargantua <martinzb949@gmail.com>
2020-12-29 09:00:45 -0800

    fixed my product-routes.js and my tag-routes.js so now my delting product route works and my tag route works

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,10 @@ router.get("/", async(req, res) => {
     try {
         const tagData = await Tag.findAll({
             // Add product as a second model to JOIN with
-            include: [{ model: ProductTag }, { model: Product }],
+            include: [{
+                model: Product,
+                through: ProductTag,
+            }, ],
         });
         res.status(200).json(tagData);
     } catch (err) {
@@ -22,7 +25,10 @@ router.get("/:id", async(req, res) => {
     // find a single tag by its `id`
     try {
         const tagData = await Tag.findByPk(req.params.id, {
-            include: [{ model: ProductTag }, { model: Product }],
+            include: [{
+                model: Product,
+                through: ProductTag,
+            }, ],
         });
         if (!tagData) {
             res.status(404).json({ message: "No tag found with that ID" });
